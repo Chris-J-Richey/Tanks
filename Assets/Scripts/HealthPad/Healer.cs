eb7f8ad179f8b423d801924ad4d5b6cc395b7073
Chris Richey <chris.j.richey@gmail.com>
2016-10-06 16:33:59 -0700

    Tanks game with a few alterations

---
+++ Assets/Scripts/HealthPad/Healer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Healer : MonoBehaviour {
+
+	public float healValue = -0.1f;
+	public float healLength = 10f;
+	public float healPause = 30f;
+
+	private float healTime;
+	private float healWait;
+	private Vector3 m_Area = new Vector3 (5f, 25f, 5f);
+
+	private void OnTriggerStay(Collider other){
+		if (Time.time < healTime && Time.time > healWait) {
+			Collider[] colliders = Physics.OverlapBox (transform.position, m_Area);
+
+			for (int i = 0; i < colliders.Length; i++) {
+				Rigidbody targetRigidbody = colliders [i].GetComponent<Rigidbody> ();
+
+				if (!targetRigidbody) {
+					continue;
+				}
+
+				TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth> ();
+
+				if (!targetHealth) {
+					continue;
+				}
+
+				targetHealth.TakeDamage (-1 * healValue);
+			}
+		}
+	}
+	private void OnTriggerEnter(Collider other){
+		healTime = Time.time + healLength;
+	}
+	private void OnTriggerExit(Collider other){
+		healWait = Time.time + healPause;
+	}
+}
